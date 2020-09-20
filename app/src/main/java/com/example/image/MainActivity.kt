973b6ae3cd68940df555e12fece806380e7c1d9e
Matias Jalava <matias@elm.fi>
2020-09-20 16:02:01 +0300

    capture of high-res images working

--- app/src/main/java/com/example/image/MainActivity.kt
+++ app/src/main/java/com/example/image/MainActivity.kt
@@ -1,42 +1,65 @@
 package com.example.image
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    val REQUEST_IMAGE_CAPTURE = 99
+    val TAG = "yayeet"
+    val fileName = "temp_photo"
 
-    val myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    lateinit var mCurrentPhotoPath: String
 
-    private val takePicture = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){
-        result -> imageView.setImageBitmap(result)
+    private val takePicture =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { result ->
+            if (result) {
+                val imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
+                imageView.setImageBitmap(imageBitmap)
+            }
+        }
+
+    private fun createUri(context: Context, authority: String, imgFile: File?): Uri? {
+        return if (imgFile != null) {
+            FileProvider.getUriForFile(
+                context,
+                authority,
+                imgFile
+            )
+        } else {
+            null
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener{
-            if(myIntent.resolveActivity(getPackageManager()) != null){
-                startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
-            }
-            // takePicture.launch(null)
-        }
-    }
+        val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        var imageFile: File? = null
+        imageFile = File.createTempFile(fileName, ".jpg", imgPath)
+        mCurrentPhotoPath = imageFile!!.absolutePath
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, recIntent: Intent?) {
-        super.onActivityResult(requestCode, resultCode, recIntent)
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
-            val extras = recIntent!!.extras
-            val imageBitmap = extras!!.get("data") as Bitmap
-            imageView.setImageBitmap(imageBitmap)
+        val photoUri = createUri(this, "com.example.image.provider", imageFile)
+
+        if (photoUri != null) {
+            button.setOnClickListener {
+                takePicture.launch(photoUri)
+                // takePicture.launch(null)
+            }
         }
+
     }
 }
